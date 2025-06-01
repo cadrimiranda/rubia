9e9dd21ed34f6756b04466b801d16902285692f1
Carlos Adriano Miranda <cadriano.miranda@gmail.com>
2025-06-01 19:52:22 -0300

    added api server

---
+++ api/src/main/java/com/ruby/rubia_server/RubiaChatServerApplication.java
@@ -0,0 +1,13 @@
+package com.ruby.rubia_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubiaChatServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RubiaChatServerApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/ruby/rubia_server/RubiaChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruby.rubia_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubiaChatServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
